0ec9d0f69d01d1ea37967677eb97227f01feb852
Tanmay Jain <cyber.tj07@gmail.com>
2021-06-26 14:10:40 +0530

    Add files via upload

---
+++ tj/js/custom.js
@@ -0,0 +1,96 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* Smoothscroll js
+  -----------------------------------------------*/
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+ /* Home Slideshow Vegas
+  -----------------------------------------------*/
+  $(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'images/main.jpg' },
+            { src: 'images/gallery-img2.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomIn', ],
+        animation: ['kenburns']
+    });
+  });
+
+    // Hero typed
+    if ($('.typed').length) {
+      var typed_strings = $(".typed").data('typed-items');
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 50,
+        backSpeed: 10,
+        backDelay: 2000
+      });
+    }
+  
+
+  /* Team carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 5,
+          itemsDesktop : [1199,5],
+          itemsDesktopSmall : [979,5],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
+    
+
+    /* Back to Top
+    -----------------------------------------------*/
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+            } else {
+                $('.go-top').fadeOut(200);
+           }
+        });   
+          // Animate the scroll to top
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
